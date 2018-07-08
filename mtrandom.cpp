7a26d9335f8ed5704789230b8a466771a7309bdf
Sasha Khapyorsky <sashakh@gmail.com>
2018-07-08 17:15:05 +0300

    mtrandom: keep statics in source rather than header file

--- mtrandom.cpp
+++ mtrandom.cpp
@@ -1,4 +1,15 @@
+//------------------------------------------------------------------------------
+#include "includes.h"
 #include "mtrandom.h"
+//------------------------------------------------------------------------------
+#define MTRANDOM_N 624
+#define MTRANDOM_M 397
+#define MTRANDOM_MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define MTRANDOM_UPPER_MASK 0x80000000UL /* most significant w-r bits */
+#define MTRANDOM_LOWER_MASK 0x7fffffffUL /* least significant r bits */
+
+static unsigned long mtrandom_mt[MTRANDOM_N]; /* the array for the state vector  */
+static int mtrandom_mti = MTRANDOM_N+1; /* mti==N+1 means mt[N] is not initialized */
 //------------------------------------------------------------------------------
 void init_genrand(unsigned long s)
 {
